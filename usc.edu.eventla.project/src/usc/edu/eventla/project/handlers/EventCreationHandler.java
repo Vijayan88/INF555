9417f59ae77d1edb6864743ba5cc854524a410a0
Ayushi Agarwal <ayushiag@usc.edu>
2016-04-14 16:40:26 -0700

    latest

--- usc.edu.eventla.project/src/usc/edu/eventla/project/handlers/EventCreationHandler.java
+++ usc.edu.eventla.project/src/usc/edu/eventla/project/handlers/EventCreationHandler.java
@@ -14,9 +14,14 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
 
+import org.bson.Document;
+
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
 import com.mongodb.Mongo;
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
 import com.mongodb.gridfs.GridFS;
 import com.mongodb.gridfs.GridFSDBFile;
 import com.mongodb.gridfs.GridFSInputFile;
@@ -26,6 +31,72 @@ public class EventCreationHandler extends HttpServlet {
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		
+		String title = request.getParameter("event_title");
+		String event_category = request.getParameter("event_category");
+		String venue = request.getParameter("venue");
+		String address1 = request.getParameter("address1");
+		String address2 = request.getParameter("address2");
+		String city_name = request.getParameter("city_name");
+		String pin_code = request.getParameter("pin_code");
+		String state = request.getParameter("state");
+		String start_date = request.getParameter("start_date");
+		String start_time = request.getParameter("start_time");
+		String end_date = request.getParameter("end_date");
+		String end_time = request.getParameter("end_time");
+		//String event_image = request.getParameter("event_image");
+		String description = request.getParameter("event_description");
+		String event_website = request.getParameter("event_website");
+		String organizer_name = request.getParameter("organiser_name");
+		String organiser_description = request.getParameter("organiser_description");
+		String facebook = request.getParameter("facebook_link");
+		String twitter_link= request.getParameter("twitter_link");
+		String ticket_type1 = request.getParameter("ticket_type1");
+		String ticket_name1 = request.getParameter("ticket_name1");
+		String quantity_present1 = request.getParameter("quantity_present1");
+		String ticket_price1 = request.getParameter("ticket_price1");
+		String ticket_type2 = request.getParameter("ticket_type2");
+		String ticket_name2 = request.getParameter("ticket_name2");
+		String quantity_present2 = request.getParameter("quantity_present2");
+		String ticket_price2 = request.getParameter("ticket_price2");
+		String ticket_type3 = request.getParameter("ticket_type1");
+		String ticket_name3 = request.getParameter("ticket_name3");
+		String quantity_present3 = request.getParameter("quantity_present3");
+		String ticket_price3 = request.getParameter("ticket_price3");
+		String event_type= request.getParameter("event_type");
+		
+		MongoClient mongoClient = new MongoClient();
+
+		MongoDatabase database = mongoClient.getDatabase("eventsla");
+		MongoCollection<Document> collection = database.getCollection("CreateEvent");
+
+		Document doc = new Document("event_title", title).append("address", new Document("venue" ,venue)
+                .append("address1", address1)
+                .append("address2", address2)
+                .append("city_name", city_name)
+                .append("state", state)
+                .append("pin_code", pin_code)
+                .append("start_date",start_date)
+                .append("start_time",start_time))
+				.append("event_description", description);
+		collection.insertOne(doc);
+		response.sendRedirect("eventdesc.html");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 		
 		final Part filePart = request.getPart("file");
 	    final String fileName = getFileName(filePart);
@@ -61,7 +132,7 @@ public class EventCreationHandler extends HttpServlet {
 		
 	    Mongo mongo = new Mongo("localhost", 27017);
 		DB db = mongo.getDB("eventsla");
-		DBCollection collection = db.getCollection("EventDetails");
+	//	DBCollection collection = db.getCollection("EventDetails");
 
 		String newFileName = "mkyong-java-image";
 
---
+++ usc.edu.eventla.project/src/usc/edu/eventla/project/handlers/EventListHandler.java
@@ -0,0 +1,52 @@
+package usc.edu.eventla.project.handlers;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class EventListHandler extends HttpServlet {
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		PrintWriter out = response.getWriter();
+		InputStream in;
+		in = getServletContext().getResourceAsStream("/eventlisttemplate1.txt");
+		BufferedReader reader = new BufferedReader(new InputStreamReader(in, "UTF-8"));
+		StringBuilder sb = new StringBuilder();
+		String line = null;
+		while ((line = reader.readLine()) != null) {
+			sb.append(line + "\n");
+		}
+		out.println(sb.toString());
+
+	
+	
+	// mongodb
+	for(int i = 0; i < 4 ; i ++){
+		in = getServletContext().getResourceAsStream("/eventlisttemplate2.txt");
+        reader = new BufferedReader(new InputStreamReader(in, "UTF-8"));
+		sb = new StringBuilder();
+		while ((line = reader.readLine()) != null) {
+			sb.append(line + "\n");
+		}
+		out.println(sb.toString());
+	}
+	in = getServletContext().getResourceAsStream("/eventlisttemplate3.txt");
+    reader = new BufferedReader(new InputStreamReader(in, "UTF-8"));
+	sb = new StringBuilder();
+	while ((line = reader.readLine()) != null) {
+		sb.append(line + "\n");
+	}
+	out.println(sb.toString());
+	
+}
+}
